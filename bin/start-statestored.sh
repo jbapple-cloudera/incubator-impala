0bcc6717f4dda01e96217f5a5efb8e77eda7b19a
Jim Apple <jbapple-impala@apache.org>
2017-03-05 16:40:15 -0800

    suppress libstdc++ enum issue
    Change-Id: Iac7a568ccc2d90ee174ccc286c108d0b28018fbb

--- bin/start-statestored.sh
+++ bin/start-statestored.sh
@@ -59,4 +59,6 @@ if ${CLUSTER_DIR}/admin is_kerberized; then
   fi
 fi
 
+export UBSAN_OPTIONS='print_stacktrace=1 suppressions=../../../../bin/MyUbSan.supp'
+export PATH="${IMPALA_TOOLCHAIN}/llvm-${IMPALA_LLVM_VERSION}/bin:${PATH}"
 exec ${BINARY_BASE_DIR}/${BUILD_TYPE}/statestore/statestored ${STATESTORED_ARGS}
