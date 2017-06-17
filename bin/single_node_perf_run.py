9f12f4c6a45e88e3b2cde5abe2b6d58c9fac53c2
Jim Apple <jbapple-impala@apache.org>
2017-06-17 13:15:09 -0700

    even more robust workload protction
    Change-Id: Ib247e231982724d641b015f6ad7355a96cd7e9f5

--- bin/single_node_perf_run.py
+++ bin/single_node_perf_run.py
@@ -231,6 +231,7 @@ def perf_ab_test(options, args):
     hash_b = get_git_hash_for_name(args[1])
     # discard any changes created by the previous restore_workloads()
     shutil.rmtree("testdata/workloads")
+    sh.git.checkout("--", "testdata/workloads")
     build(hash_b, options)
     restore_workloads(workload_dir)
     start_impala(options.num_impalads)
@@ -299,11 +300,14 @@ def main():
     current_hash = get_git_hash_for_name("HEAD")
 
   try:
+    workloads = backup_workloads()
     perf_ab_test(options, args)
   finally:
     # discard any changes created by the previous restore_workloads()
     shutil.rmtree("testdata/workloads")
+    sh.git.checkout("--", "testdata/workloads")
     sh.git.checkout(current_hash)
+    restore_workloads(workloads)
 
 
 if __name__ == "__main__":
