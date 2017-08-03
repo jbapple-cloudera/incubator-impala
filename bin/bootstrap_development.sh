7dcffe52a0b06a8df2cb893f3a29600c8815cdf0
Jim Apple <jbapple-impala@apache.org>
2017-08-03 08:27:36 -0700

    simplify
    Change-Id: I5d133d4213dd4b2d0a336e8fc62a6341aaf9da79

--- bin/bootstrap_development.sh
+++ bin/bootstrap_development.sh
@@ -17,16 +17,15 @@
 # specific language governing permissions and limitations
 # under the License.
 
-
 # This script bootstraps a development environment from almost nothing; it is known to
-# work on Ubuntu 14.04, and it definitely clobbers some local environment, so it's best to
-# run this in a sandbox first, like a VM or docker.
+# work on Ubuntu 14.04 and 16.04. It clobbers some local environment, so it is best to
+# run this in a fresh install.
 #
 # The intended user is a person who wants to start contributing code to Impala. This
 # script serves as an executable reference point for how to get started.
 #
-# At this time, it completes in about 6.5 hours. It generates and loads the test data and
-# metadata without using a snapshot (which takes about 3 hours) and it then runs the full
+# At this time, it completes in about 4 hours. It generates and loads the test data and
+# metadata without using a snapshot (which takes about 2 hours) and it then runs the full
 # testsuite (frontend, backend, end-to-end, JDBC, and custom cluster) in "core"
 # exploration mode.
 
@@ -34,9 +33,8 @@ set -eux -o pipefail
 
 sudo apt-get update
 
-sudo apt-get --yes install ccache ninja-build git g++ gcc git \
-     libsasl2-dev libssl-dev make maven python-dev python-setuptools postgresql \
-     liblzo2-dev ntp ntpdate
+sudo apt-get --yes install ccache ninja-build g++ gcc git libsasl2-dev libssl-dev make \
+     maven python-dev python-setuptools postgresql liblzo2-dev ntp ntpdate
 
 if lsb_release -d | grep "Ubuntu 16.04"
 then
@@ -56,20 +54,23 @@ fi
 sudo service ntp restart
 sudo service ntp stop
 sudo ntpdate us.pool.ntp.org
+# If on EC2, use Amazon's ntp servers
 if [ -f /sys/hypervisor/uuid ] && [ `head -c 3 /sys/hypervisor/uuid` == ec2 ]
 then
   sudo sed -i 's/ubuntu\.pool/amazon\.pool/' /etc/ntp.conf
+  grep amazon /etc/ntp.conf
+  grep ubuntu /etc/ntp.conf
 fi
-grep amazon /etc/ntp.conf
-grep ubuntu /etc/ntp.conf
 sudo service ntp start
 
 # TODO: config ccache
 
 # TODO: check that there is enough space on disk to do a data load
+
+# If there is no Impala git repo, get one now
 if ! test -d ~/Impala
 then
-  time -p git clone http://gerrit.cloudera.org:8080/Impala-ASF ~/Impala
+  time -p git clone //git-wip-us.apache.org/repos/asf/incubator-impala.git
 fi
 cd ~/Impala
 
@@ -136,54 +137,3 @@ fi
 source bin/impala-config.sh
 export NUM_CONCURRENT_TESTS=$(nproc)
 time -p ./buildall.sh -noclean -format -testdata
-echo $?
-
-# sudo -u postgres psql -c "DROP DATABASE IF EXISTS sentry_policy" postgres && sudo -u postgres psql -c "DROP DATABASE IF EXISTS hive_impala" postgres && sudo -u postgres psql -c "DROP ROLE hiveuser" postgres && sudo rm -rf /var/lib/hadoop-hdfs/ && rm -rf .ssh/id_rsa* && rm -rf ~/*
-# sudo -u postgres psql -c "DROP DATABASE IF EXISTS sentry_policy" postgres && sudo -u postgres psql -c "DROP DATABASE IF EXISTS hive_impala" postgres && (sudo -u postgres psql -c "DROP ROLE hiveuser" postgres || echo "no role") && sudo rm -rf /var/lib/hadoop-hdfs/ && rm -rf .ssh/id_rsa* && rm -rf ~/*
-# HOMEDIR="/home/$(whoami)/"
-
-# if [[ ! -d "${HOMEDIR}" ]]
-# then
-#     echo "${HOMEDIR} is needed for installing Impala dependencies"
-#     exit 1
-# fi
-
-# if [[ -z "${JAVA_HOME}" ]]
-# then
-#     echo "JAVA_HOME must be set to install Impala dependencies"
-#     exit 1
-# fi
-
-# if ! sudo true
-# then
-#     echo "Passwordless sudo is needed for this script"
-#     exit 1
-# fi
-
-# IMPALA_SETUP_REPO_URL="https://github.com/awleblang/impala-setup"
-
-# # Place to download setup scripts
-# TMPDIR=$(mktemp -d)
-# function cleanup {
-#     rm -rf "${TMPDIR}"
-# }
-# trap cleanup EXIT
-
-# # Install build and test pre-reqs
-# pushd "${TMPDIR}"
-# git clone "${IMPALA_SETUP_REPO_URL}" impala-setup
-# cd impala-setup
-# chmod +x ./install.sh
-# sudo ./install.sh
-# popd
-
-# # HDFS bug workaround
-# echo "127.0.0.1 $(hostname -s) $(hostname)" | sudo tee -a /etc/hosts
-# echo "NoHostAuthenticationForLocalhost yes" >> ~/.ssh/config
-
-# pushd "$(dirname $0)/.."
-# export IMPALA_HOME="$(pwd)"
-# export MAX_PYTEST_FAILURES=0
-# source bin/impala-config.sh
-# ./buildall.sh -noclean -format -testdata -build_shared_libs
-# popd
